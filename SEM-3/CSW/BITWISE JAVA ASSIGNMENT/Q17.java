1c0dd18ee6a1c19ec517f4a9fc54241c7e21194e
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2025-01-06 10:59:45 +0530

    Create Q17.java

---
+++ SEM-3/CSW/BITWISE JAVA ASSIGNMENT/Q17.java
@@ -0,0 +1,10 @@
+public class Q17 {
+    public static void main(String[] args) {
+        int number = 4; // Change this to test other numbers
+        System.out.println(isEven(number));
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0; // Returns true if num is even, false otherwise
+    }
+}
