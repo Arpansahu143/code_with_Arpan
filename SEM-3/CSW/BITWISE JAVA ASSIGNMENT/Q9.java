3f758a9e2382a6cff323df28b838ae374a68ed9c
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2025-01-06 10:59:27 +0530

    Create Q9.java

---
+++ SEM-3/CSW/BITWISE JAVA ASSIGNMENT/Q9.java
@@ -0,0 +1,24 @@
+public class Q9 {
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10; // Get the last digit
+            reversed = reversed * 10 + digit; // Append the digit to the reversed number
+            number /= 10; // Remove the last digit from the original number
+        }
+
+        return originalNumber == reversed;
+    }
+
+    public static void main(String[] args) {
+        int number = 121;
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+}
