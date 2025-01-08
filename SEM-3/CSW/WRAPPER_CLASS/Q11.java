685408097ee034c16db63a6322e6c9d8e0c45701
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2025-01-08 21:45:39 +0530

    Update Q11.java

--- SEM-3/CSW/WRAPPER_CLASS/Q11.java
+++ SEM-3/CSW/WRAPPER_CLASS/Q11.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Q11 {
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-System.out.print("Enter a double: ");
-String inputString = scanner.nextLine(); // Read input as a string
-double doubleValue = Double.parseDouble(inputString); // Using parseDouble method
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a double: ");
+        String inputString = scanner.nextLine(); // Read input as a string
+        double doubleValue = Double.parseDouble(inputString); // Using parseDouble method
 
-System.out.println("The double value is: " + doubleValue);
-scanner.close();
-}
+        System.out.println("The double value is: " + doubleValue);
+        scanner.close();
+    }
 }
