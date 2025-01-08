038c0dc9c6077b27ef3c186a367774c308fddfa5
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2025-01-08 19:50:53 +0530

    Update Q8.java

--- SEM-3/CSW/BITWISE JAVA ASSIGNMENT/Q8.java
+++ SEM-3/CSW/BITWISE JAVA ASSIGNMENT/Q8.java
@@ -1,23 +1,21 @@
+import java.util.Scanner;
 public class Q8 {
-    public static int reverse(int number) {
-        int reversed = 0;
-        int sign = number < 0 ? -1 : 1;
-        number = Math.abs(number);
-
-        while (number != 0) {
-            int digit = number % 10;
-            reversed = reversed * 10 + digit;
-            number /= 10;
-        }
-
-        return reversed * sign;
-    }
-
-    public static void main(String[] args) {
-        int input1 = 123;
-        int input2 = -245;
-
-        System.out.println("Reversed " + input1 + " is: " + reverse(input1)); // Outputs: 321
-        System.out.println("Reversed " + input2 + " is: " + reverse(input2)); // Outputs: -542
-    }
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter an integer: ");
+int number = scanner.nextInt();
+int reversedNumber = reverse(number);
+System.out.println("The reverse of " + number + " is: " + reversedNumber);
+scanner.close();
+}
+public static int reverse(int num) {
+int reversed = 0;
+int sign = (num < 0) ? -1 : 1;
+num = Math.abs(num);
+while (num != 0) {
+int digit = num % 10;
+reversed = reversed * 10 + digit;
+num /= 10; }
+return reversed * sign;
+}
 }
