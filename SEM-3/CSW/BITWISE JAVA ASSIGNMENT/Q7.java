12d3dadfece38732e98a8e236c6b3d67eb64fbc6
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2025-01-08 19:39:57 +0530

    Update Q7.java

--- SEM-3/CSW/BITWISE JAVA ASSIGNMENT/Q7.java
+++ SEM-3/CSW/BITWISE JAVA ASSIGNMENT/Q7.java
@@ -1,20 +1,30 @@
+import java.util.Scanner;
 public class Q7 {
-    public static int power(int x, int y) {
-        int result = 1;
-
-        // Loop y times to multiply x
-        for (int i = 0; i < y; i++) {
-            result *= x;
-        }
-
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int x = 2;
-        int y = 5;
-
-        int result = power(x, y);
-        System.out.println(x + "^" + y + " = " + result);
-    }
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter the base (x): ");
+double x = scanner.nextDouble();
+System.out.print("Enter the exponent (y): ");
+int y = scanner.nextInt();
+double result = power(x, y);
+System.out.println(x + " raised to the power of " + y + " is: " + result);
+scanner.close();
+}
+public static double power(double x, int y) {
+if (y == 0) {
+return 1; // x^0 is 1
+}
+if (y < 0) {
+x = 1 / x;
+y = -y; }
+double result = 1;
+while (y > 0) {
+if ((y & 1) == 1) {
+result *= x;
+}
+x *= x;
+y >>= 1;
+}
+return result;
+}
 }
