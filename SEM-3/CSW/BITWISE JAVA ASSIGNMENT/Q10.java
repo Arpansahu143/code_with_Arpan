5fe03ac7f98b69107a47219ddf6d1e1f40bb80a5
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2025-01-09 07:28:17 +0530

    Update Q10.java

--- SEM-3/CSW/BITWISE JAVA ASSIGNMENT/Q10.java
+++ SEM-3/CSW/BITWISE JAVA ASSIGNMENT/Q10.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class Q10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the first float number: ");
         float num1 = scanner.nextFloat();
-
         System.out.print("Enter the second float number: ");
         float num2 = scanner.nextFloat();
+        float epsilon = 0.0001f; // You can adjust this value as needed, but it should be less than 1
 
-        System.out.print("Enter the threshold value: ");
-        float threshold = scanner.nextFloat();
-
-        float difference = Math.abs(num1 - num2);
-
-        if (difference < threshold) {
-            System.out.println("The difference (" + difference + ") is less than the threshold (" + threshold + ").");
+        boolean isDifferenceLessThanEpsilon = isDifferenceLessThanEpsilon(num1, num2,
+                epsilon);
+        if (isDifferenceLessThanEpsilon) {
+            System.out.println("The difference between " + num1 + " and " + num2 + " is less than " + epsilon + ".");
         } else {
-            System.out.println("The difference (" + difference + ") is not less than the threshold (" + threshold + ").");
+            System.out
+                    .println("The difference between " + num1 + " and " + num2 + " is not less than " + epsilon + ".");
         }
-
         scanner.close();
+    }
+
+    public static boolean isDifferenceLessThanEpsilon(float a, float b, float epsilon) {
+        return Math.abs(a - b) < epsilon;
     }
 }
